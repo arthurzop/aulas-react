dc492328e854230365d1a669f1a0700129025d7b
Arthur <arthurferreira1417@gmail.com>
2024-05-14 21:57:31 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css"
+
+export default function App() {
+  return (
+    <body>
+      <div className="card" >
+        <h1>React App</h1>
+        <p>Front-End JS</p>
+      </div>
+    </body>
+  );
+}
